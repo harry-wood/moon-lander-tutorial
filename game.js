c8fb84cc0445c7f08de72f2f703d275bdd462609
Harry Wood <harry.wood@opencorporates.com>
2020-10-17 23:38:20 +0100

    Resize canvas and everything relative to canvas

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ function drawSpaceship() {
         context.beginPath();
         context.moveTo(spaceship.width * -0.5, spaceship.height * 0.5);
         context.lineTo(spaceship.width * 0.5, spaceship.height * 0.5);
-        context.lineTo(0, spaceship.height * 0.5 + Math.random() * 10);
+        context.lineTo(0, spaceship.width * 1.5 + Math.random() * spaceship.width);
         context.lineTo(spaceship.width * -0.5, spaceship.height * 0.5);
         context.closePath();
         context.fillStyle = "orange";
@@ -42,7 +42,7 @@ function drawStars() {
   for (var i = 0; i < stars.length; i++) {
     var star = stars[i];
     context.beginPath();
-    context.arc(star.x, star.y, star.radius, 0, 2*Math.PI);
+    context.arc(star.x * canvas.width, star.y * canvas.height, star.radius, 0, 2*Math.PI);
     context.closePath();
     context.fillStyle = "rgba(255, 255, 255, " + star.alpha + ")";
     if (star.decreasing == true) {
@@ -80,30 +80,26 @@ function keyPressed(event) {
 document.addEventListener('keyup', keyLetGo);
 document.addEventListener('keydown', keyPressed);
 
-var canvas = document.getElementById("game");
+var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
 
 var spaceship = {
   color: "white",
-  width: 8,
-  height: 22,
-  thrust: -0.1,
-  position: { x: 50, y: 50 },
-  velocity: { x: 0.2, y: -1 },
+  position: { x: 0.2 * canvas.width, y: 0.2 * canvas.width},
+  velocity: { x: 0.001 * canvas.width, y: -0.002 * canvas.width },
   angle: 0,
   engineOn: false,
   rotatingLeft: false,
   rotatingRight: false,
   crashed: false
 }
-
-var gravity = -0.03;
+var gravity;
 var stars = [];
 
 for (var i = 0; i < 500; i++) {
   stars[i] = {
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
+    x: Math.random(),
+    y: Math.random(),
     radius: Math.sqrt(Math.random() * 2),
     alpha: 1.0,
     decreasing: true,
@@ -111,4 +107,39 @@ for (var i = 0; i < 500; i++) {
   };
 }
 
+resizeGame();
 draw();
+
+
+window.addEventListener('resize', resizeGame, false);
+window.addEventListener('orientationchange', resizeGame, false);
+
+function resizeGame() {
+  var gameArea = document.getElementById('gameArea');
+  var widthToHeight = 4 / 3;
+  var newWidth = window.innerWidth;
+  var newHeight = window.innerHeight;
+  var newWidthToHeight = newWidth / newHeight;
+
+  if (newWidthToHeight > widthToHeight) {
+    newWidth = newHeight * widthToHeight;
+    gameArea.style.height = newHeight + 'px';
+    gameArea.style.width = newWidth + 'px';
+  } else {
+    newHeight = newWidth / widthToHeight;
+    gameArea.style.width = newWidth + 'px';
+    gameArea.style.height = newHeight + 'px';
+  }
+
+  gameArea.style.marginTop = (-newHeight / 2) + 'px';
+  gameArea.style.marginLeft = (-newWidth / 2) + 'px';
+
+  var gameCanvas = document.getElementById('gameCanvas');
+  gameCanvas.width = newWidth;
+  gameCanvas.height = newHeight;
+
+  spaceship.width = 0.03 * canvas.width;
+  spaceship.height = spaceship.width * 2;
+  spaceship.thrust = canvas.width * -0.00005;
+  gravity = -0.00001 * canvas.width;
+}
